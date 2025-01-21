88b8cbb363d1639fe427ec84d5037f4442d99687
“zmastropiero” <zmastropiero@gmail.com>
2025-01-20 22:12:47 -0500

    roll bck connections:

--- connections.py
+++ connections.py
@@ -6,7 +6,6 @@ from urllib.parse import quote
 import os
 import subprocess
 from dotenv import load_dotenv
-import sys
 
 load_dotenv()
 
@@ -148,25 +147,15 @@ def run_lftp(
                 bye
                 """
     try:
-        # Use subprocess.Popen to capture real-time output
-        process = subprocess.Popen(
-            ["lftp", "-d", "-c", lftp_script],
+        result = subprocess.run(
+            ["lftp", "-c", lftp_script],
             text=True,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE
+            capture_output=True,
+            check=True,
         )
-
-        # Stream stdout and stderr to console in real-time
-        for line in process.stdout:
-            print(line, end='')
-
-        for line in process.stderr:
-            print(line, end='', file=sys.stderr)
-
-        process.wait()  # Wait for process to complete
-        if process.returncode == 0:
-            print("LFTP command executed successfully.")
-        else:
-            print(f"LFTP command failed with return code {process.returncode}.")
-    except Exception as e:
-        print(f"An error occurred: {e}")
+        print("LFTP command executed successfully.")
+        print(result.stdout)
+    except subprocess.CalledProcessError as e:
+        print("Error executing lftp command.")
+        print(e.stderr)
+        raise
