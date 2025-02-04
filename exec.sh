00089af2fd149e61536af7fbc19216fdb94e10b3
Zachary Mastropiero <kr@Zacharys-Mac-mini.local>
2025-02-03 19:26:58 -0500

    final commit

--- exec.sh
+++ exec.sh
@@ -6,7 +6,7 @@ source ~/Projects/media_server/venv/bin/activate
 # Infinite loop to restart the script if it exits
 while true; do
     echo "Starting media_server.py..."
-    python3 ~/Projects/media_server/src/media_server.py
+    python3 /Users/kr/Projects/media_server/src/qb_api.py
     echo "media_server.py exited. Restarting in 5 seconds..."
     sleep 30
 done
